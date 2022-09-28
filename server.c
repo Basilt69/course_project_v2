e63b5c76fc67759c0ca852087e08fedc5dc2d691
basiltkachenko <baziltkachenko@gmail.com>
2022-09-28 17:16:53 +0300

    threads added_2

--- server.c
+++ server.c
@@ -74,3 +74,49 @@ int check(int exp, const char *msg){
     }
     return exp;
 }
+
+void * handle_connection(void* p_client_socket) {
+    int client_socket = *((int*)p_client_socket);
+    free(p_client_socket);
+    char buffer[BUFSIZE];
+    size_t bytes_read;
+    int msgsize = 0;
+    char actualpath[PATH_MAX+1];
+
+    // read the client's message -- the name of the file to read
+    while((bytes_read = read(client_socket, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0) {
+        msgsize += bytes_read;
+        if (msgsize > BUFSIZE-1 || buffer[msgsize-1] == '\n') break;
+    }
+    check(bytes_read,"recv error");
+    buffer[msgsize-1] =0; // null terminate the message and remove the \n
+
+    printf("REQUEST: %s\n", buffer);
+    fflush(stdout);
+
+    //validity check
+    if (realpath(buffer, actualpath) == NULL){
+        printf("ERROR(bad path): %s\n",buffer);
+        close(client_socket);
+        return NULL;
+    }
+
+    // read file and send its contents to client
+    FILE *fp = fopen(actualpath, "r");
+    if (fp == NULL){
+        printf("ERRPR(open): %s\n",buffer);
+        close(client_socket);
+        return NULL;
+    }
+    sleep(1);
+
+    // read file contents and send them to client
+    while((bytes_read = fread(buffer, 1, BUFSIZE, fp)) > 0){
+        printf("sending %zu bytes\n",bytes_read);
+        write(client_socket, buffer,bytes_read);
+    }
+    close(client_socket);
+    fclose(fp);
+    printf("closing connection\n");
+    return NULL;
+}
