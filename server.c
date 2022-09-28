004b771909fe02f1e66d8eac3fd9d7e6469f883d
basiltkachenko <baziltkachenko@gmail.com>
2022-09-28 13:55:30 +0300

    initial commit

---
+++ server.c
@@ -0,0 +1,11 @@
+// a new thread for every conncection
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
